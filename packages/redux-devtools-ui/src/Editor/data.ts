b0dc4dfaa6a45fd93e7b65a7c5bc7725b048b3f7
Hector Hernandez <hsanchez@etsy.com>
2023-05-17 15:13:06 -0400

    adds mock data

--- packages/redux-devtools-ui/src/Editor/data.ts
+++ packages/redux-devtools-ui/src/Editor/data.ts
@@ -1,2 +1,123 @@
-const dv = ``
+const dv = `{
+    {
+        fullName: "Lynette Stark",
+        address:
+        {
+            street: "505 Mills Avenue",
+            city: "Gustavia"
+        }
+    },
+    {
+        fullName: "Justin Olson",
+        address:
+        {
+            street: "73690 Amelia Gardens",
+            city: "Pangai"
+        }
+    },
+    {
+        fullName: "Doreen Stark",
+        address:
+        {
+            street: "0932 Hermann Stream",
+            city: "Bujumbura"
+        }
+    },
+    {
+        fullName: "Kenneth Macejkovic",
+        address:
+        {
+            street: "267 Caroline Brook",
+            city: "Rhar el Melah"
+        }
+    },
+    {
+        fullName: "Gerardo Lakin",
+        address:
+        {
+            street: "45623 Hoppe Locks",
+            city: "Upper Hell's Gate"
+        }
+    },
+    {
+        fullName: "Diana Nolan",
+        address:
+        {
+            street: "9109 Destany Road",
+            city: "Ras al-Khaimah"
+        }
+    },
+    {
+        fullName: "Ignacio Von",
+        address:
+        {
+            street: "72735 Welch Harbor",
+            city: "Ecilda Paullier"
+        }
+    },
+    {
+        fullName: "Lynda Metz",
+        address:
+        {
+            street: "59702 Leannon Knolls",
+            city: "Чучер - Сандево"
+        }
+    },
+    {
+        fullName: "Evelyn Altenwerth",
+        address:
+        {
+            street: "9059 Harber Pass",
+            city: "Skudai"
+        }
+    },
+    {
+        fullName: "Andre Kuhlman",
+        address:
+        {
+            street: "8006 Mann Crest",
+            city: "Sint Michiel Liber"
+        }
+    },
+    {
+        fullName: "Natasha Wyman",
+        address:
+        {
+            street: "053 Breitenberg Gardens",
+            city: "The Valley"
+        }
+    },
+    {
+        fullName: "Jeanne Volkman",
+        address:
+        {
+            street: "64564 Cole Road",
+            city: "Holhol"
+        }
+    },
+    {
+        fullName: "Martin Rau",
+        address:
+        {
+            street: "12635 Hickle Lock",
+            city: "Hemnesberget"
+        }
+    },
+    {
+        fullName: "Paula Luettgen",
+        address:
+        {
+            street: "8707 Glen Streets",
+            city: "Trashi Yangtse"
+        }
+    },
+    {
+        fullName: "Derek Anderson",
+        address:
+        {
+            "street": "0570 Javonte Inlet",
+            "city": "Hóc Môn"
+        }
+    }
+  }`
 export default dv;
